243eda20f111bd85cefae0e3825e494f280dc8d3
Novel Martinez <martineznob@media-saturn.com>
2025-04-10 01:16:24 +0200

    Cleaned up code

---
+++ mockData/mockBackendOpenOrders.ts
@@ -0,0 +1,82 @@
+export const mockBackendOpenOrders = [
+  {
+    BestellNr: 20688504,
+    Erstelldatum: '2019-06-04 21:10:31.000',
+    ArtikelNr: 1901399,
+    Hrs: 'RAVENSCOURT',
+    Artikel: 'NSW VOICE OF GERMANY 19+2MIC /D',
+    WgrNo: '8123', // Changed to string
+    Anzahl: 1,
+    BestellStatus: 'UNKNOWN',
+  },
+  {
+    BestellNr: 20802494,
+    Erstelldatum: '2019-07-27 23:49:30.000',
+    ArtikelNr: 1909288,
+    Hrs: 'TONIES',
+    Artikel: '01-0178 30 LIEBLINS KINDERLIEDER SC',
+    WgrNo: '450', // Changed to string
+    Anzahl: 1,
+    BestellStatus: 'ORDER_COMPLETED',
+  },
+  {
+    BestellNr: 20985737,
+    Erstelldatum: '2019-10-28 20:09:03.000',
+    ArtikelNr: 1106030,
+    Hrs: 'NESCAFÉ',
+    Artikel: '12385198 LUNGO DECAFFEINATO 16C',
+    WgrNo: '629', // Changed to string
+    Anzahl: 2,
+    BestellStatus: 'UNKNOWN',
+  },
+  {
+    BestellNr: 21105315,
+    Erstelldatum: '2019-11-29 22:13:39.000',
+    ArtikelNr: 1592944,
+    Hrs: 'NUTRIBULLET',
+    Artikel: 'EXTRAKTOR 600W 12PCS BLACK DEAL',
+    WgrNo: '674', // Changed to string
+    Anzahl: 1,
+    BestellStatus: 'UNKNOWN',
+  },
+  {
+    BestellNr: 21170795,
+    Erstelldatum: '2019-12-18 08:54:01.000',
+    ArtikelNr: 1802500,
+    Hrs: 'LITEON',
+    Artikel: 'ES1 DVDRW 8XSLIM USB BLACK',
+    WgrNo: '3330', // Changed to string
+    Anzahl: 1,
+    BestellStatus: 'UNKNOWN',
+  },
+  {
+    BestellNr: 21336783,
+    Erstelldatum: '2020-03-19 13:39:03.000',
+    ArtikelNr: 1977567,
+    Hrs: 'SOLIS',
+    Artikel: '8453 SILVER',
+    WgrNo: '617', // Changed to string
+    Anzahl: 1,
+    BestellStatus: 'UNKNOWN',
+  },
+  {
+    BestellNr: 21434994,
+    Erstelldatum: '2020-04-03 19:49:40.000',
+    ArtikelNr: 1780852,
+    Hrs: 'LOGITECH',
+    Artikel: 'BRIO 4K BULK',
+    WgrNo: '3620', // Changed to string
+    Anzahl: 1,
+    BestellStatus: 'UNKNOWN',
+  },
+  {
+    BestellNr: 21570024,
+    Erstelldatum: '2020-04-29 14:09:05.000',
+    ArtikelNr: 1929567,
+    Hrs: 'MICROSOFT',
+    Artikel: 'ELITE WL SERIES 2',
+    WgrNo: '3128', // Changed to string
+    Anzahl: 1,
+    BestellStatus: 'UNKNOWN',
+  },
+]
